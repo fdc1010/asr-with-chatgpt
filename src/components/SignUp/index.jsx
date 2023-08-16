58dceb7790b04eb8dd027ae094a9a8002481f666
franklin caruana <franklincaruana@gmail.com>
2023-08-16 19:03:59 +0800

    chore: finalizing scripts

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,15 +1,19 @@
 import { OrangeButtonPill, SmokeButtonPill } from 'components'
 import { noop } from 'lodash'
 import PropTypes from 'prop-types'
-const SignUp = ({ onHandleClick = noop }) => {
+import { useNavigate } from 'react-router-dom'
+const SignUp = ({ onHandleSignUp = noop }) => {
+  const navigate = useNavigate()
   return (
     <div className="w-full py-10 my-auto flex flex-row flex-wrap box-border items-start justify-center gap-[20px] text-sm text-whitesmoke-100">
       <OrangeButtonPill
-        onHandleClick={onHandleClick}
+        onHandleClick={onHandleSignUp}
         className="text-sm font-semibold w-full h-full flex flex-col items-center justify-center">
         sign up / log in
       </OrangeButtonPill>
-      <SmokeButtonPill className="text-sm font-semibold w-full h-full flex flex-col items-center justify-center">
+      <SmokeButtonPill
+        onHandleClick={() => navigate('/howitworks')}
+        className="text-sm font-semibold w-full h-full flex flex-col items-center justify-center">
         see how it works
       </SmokeButtonPill>
     </div>
