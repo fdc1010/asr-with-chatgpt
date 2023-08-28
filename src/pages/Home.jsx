f0b589b43289337e4196ce685d78935e84ca02b5
franklin caruana <franklincaruana@gmail.com>
2023-08-28 16:59:59 +0800

    fix transcribing state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
     async function callChatGPT() {
       return await axios.get(`${import.meta.env.VITE_API_BASEURL_PY}/chatgpt?prompt=${prompt}`)
     }
-    if (isTranscribing && !isRecording) {
+    if (isTranscribing) {
       callChatGPT().then((response) => {
         onHandleOutput(response.data, true)
         onHandleTrans(false)
